ba854115bbd0ede17c1617f57d298f323902ff57
kourggos <guilhermedhfontoura@gmail.com>
2024-11-29 03:10:01 -0300

    Create simulator_code.py
    codigo para simular os circuitos no repositorio

---
+++ simulator_code.py
@@ -0,0 +1,12 @@
+from qiskit_aer import AerSimulator
+from qiskit import transpile
+
+backend1 = AerSimulator()
+qc1 = transpile(circuito, backend=backend1)
+
+from qiskit.primitives import StatevectorSampler
+
+statevectorsampler = StatevectorSampler()
+pub = (qc1)
+job = statevectorsampler.run([pub])
+print(job.result()[0].data.resultado.get_int_counts())
